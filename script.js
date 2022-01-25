cf99f69350da2277df91b316a27eb73d9a72c973
SofiS41 <78041431+SofiS41@users.noreply.github.com>
2022-01-25 21:17:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,66 @@
 
-function CoffeeMaker(time, type){
-    this.timeMake = time;
-    this.typeOfMachine = type;
-}
-CoffeeMaker.prototype.on = function(){
-    console.log("The machine are waking up. Plese, waite 3 seconds");
-}
-CoffeeMaker.prototype.off = function(){
-    console.log("Your coffee is ready. Have a nice day :)");
-}
-CoffeeMaker.prototype.making = function(){
-    
-    console.log(`The ${this.typeOfMachine} maker are making your coffee. Please wait ... ${this.timeMake}s`);
-    setTimeout(()=>{
-        this.off();
-    }, this.timeMake*1000)
+let CoffeeMaker = {
+    timeMake: 1,
+    typeOfMachine: 'unknown',
+    on: function(){
+        console.log("The machine are waking up. Plese, waite 3 seconds");
+    },
+    off: function(){
+        console.log("Your coffee is ready. Have a nice day :)");
+    },
+    making: function(){
+        console.log(`The ${this.typeOfMachine} maker are making your coffee. Please wait ... ${this.timeMake}s`);
+        setTimeout(()=>{
+            this.off();
+        }, this.timeMake*1000)
+    }
 }
 
-function DripMachine(){
-    CoffeeMaker.call(this, 10, 'drip');
-    this.drip = function(){
+
+let drip = {
+    __proto__: CoffeeMaker,
+    timeMake: 10,
+    typeOfMachine: 'drip',
+    choose: function(){
         console.log("You choose drip maker");
     }
 }
-function CarobMachine(){
-    CoffeeMaker.call(this, 15, 'carob');
-    this.carob = function(){
+let carob = {
+    __proto__: CoffeeMaker,
+    timeMake: 15,
+    typeOfMachine: 'carob',
+    choose: function(){
         console.log("You choose carob maker");
     }
 }
-function CoffeeMachine(){
-    CoffeeMaker.call(this, 5, 'coffee-machine');
-    this.coffeeMachine = function(){
+let coffeeMachine = {
+    __proto__: CoffeeMaker,
+    timeMake: 5,
+    typeOfMachine: 'coffee-machine',
+    choose: function(){
         console.log("You choose coffee machine");
     }
 }
 
-DripMachine.prototype = Object.create(CoffeeMaker.prototype);
-DripMachine.prototype.constuctor = DripMachine;
-
-CarobMachine.prototype = Object.create(CoffeeMaker.prototype);
-CarobMachine.prototype.constuctor = CarobMachine;
-
-CoffeeMachine.prototype = Object.create(CoffeeMaker.prototype);
-CoffeeMachine.prototype.constuctor = CoffeeMachine;
-
 function processing(type){
+    type.choose();
     type.on();
     setTimeout(()=>{
         type.making();
     }, 3000)
 }
 
-let machine = new CoffeeMaker();
-let drip = new DripMachine();
-let carob = new CarobMachine();
-let coffeeMachine = new CoffeeMachine();
-
 let types = document.querySelector('.types');
 
 types.addEventListener('click', (e)=>{
     if(e.target.classList.contains('type')){
         if(e.target.dataset.type == 'drip'){
-            drip.drip();
             processing(drip);
         }
         else if(e.target.dataset.type == 'carob'){
-            carob.carob();
             processing(carob);
         }
         else if(e.target.dataset.type == 'coffee-machine'){
-            coffeeMachine.coffeeMachine();
             processing(coffeeMachine);
         }
     }
